ca2e57b4e4ccbf94828ed8258552cafa3aeadce6
Jonas Ebel <jonas.ebel@tum.de>
2019-06-12 21:37:23 +0200

    Add a Datamodel module

---
+++ src/model/dataModel.js
@@ -0,0 +1,11 @@
+const deliveryClient = require('./DeliveryClient');
+const deliveryGood = require('./DeliveryClient');
+const driver = require('./Driver');
+const location = require('./Location');
+const user = require('./User');
+const route = require('./Route');
+const vehicle = require('./Vehicle');
+
+module.exports = {
+    deliveryClient,deliveryGood, driver, location, user, route, vehicle
+}
