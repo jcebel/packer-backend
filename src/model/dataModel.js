fcf639a259c39a89e7c873b3650d34010eaaa65f
Jonas Ebel <jonas.ebel@tum.de>
2019-06-17 15:11:06 +0200

    Fix datamodel.js's referencing

--- src/model/dataModel.js
+++ src/model/dataModel.js
@@ -1,5 +1,5 @@
 const deliveryClient = require('./DeliveryClient');
-const deliveryGood = require('./DeliveryClient');
+const deliveryGood = require('./DeliveryGood');
 const driver = require('./Driver');
 const user = require('./User');
 const route = require('./Route');
