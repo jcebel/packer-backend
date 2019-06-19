a85f649e4dee7c504b114475473f61b60f2b4ab2
Lukas <lukas.vorwerk@tum.de>
2019-06-19 11:26:02 +0200

    check if request body is empty in create function

--- src/controllers/deliveryGood.js
+++ src/controllers/deliveryGood.js
@@ -12,6 +12,10 @@ const list  = (req, res) => {
 };
 
 const create = (req, res) => {
+    if (Object.keys(req.body).length === 0) return res.status(400).json({
+        error: 'Bad Request',
+        message: 'The request body is empty'
+    });
 
     DeliveryGoodModel.create(req.body)
         .then(deliveryGood => {
