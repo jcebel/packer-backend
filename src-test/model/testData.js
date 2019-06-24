c918f5bdcf70ce52d6d9c582c1c2cafafdd1c825
Jonas Ebel <jonas.ebel@tum.de>
2019-06-24 13:35:18 +0200

    Adapt testdata

--- src-test/model/testData.js
+++ src-test/model/testData.js
@@ -101,47 +101,68 @@ const executeTest = function() {
         }).catch(handleErr);
     }).catch(handleErr);
 
-    //add vehicle
-    const vehicle = new model.vehicle({
-        maxDistance: 10,
-        maxSize: 15,
-        maxItems: 8 });
-    vehicle.save().then( function(veh) {
-        const driver = new model.driver({driverLicenseNumber: "abcde12345", isAvailable: true, vehicle: veh._id});
-        driver.save().then(function (driv) {
-            const seppDriver = new model.user({
-                name: "Sepp",
-                lastName: "Müller",
-                birthday: new Date('1995-05-08T03:24:00'),
-                homeAddress: {
-                    city: "Muenchen",
-                    street: "Implerstraße",
-                    houseNumber: 1,
-                    postalCode: "81371"
-                }
-            });
-            seppDriver.driver = driv._id;
-            seppDriver.save().then(function (seppUser) {
-                const route = new model.route({
-                    kilometers: 10,
-                    estimatedArrivalTimes: [new Date('2019-06-20T03:24:00')],
-                    items: [dishwasher, bike],
-                    auctionBids: [{
-                        owner: driver._id,
-                        bid: 4,
-                        timestamp: new Date('2019-06-19T03:24:00')
-                    }]
+    //other driver
+
+    const driver2 = new model.driver({driverLicenseNumber: "üpoiuzkn123123", isAvailable: true})
+    driver2.save().then(function(driv) {
+        return new model.user({
+            name: "Franz",
+            lastName: "Xaver",
+            birthday: new Date('1996-05-08T03:24:00'),
+            homeAddress: {
+                city: "Muenchen",
+                street: "Leopoldstrasse",
+                houseNumber: 1,
+                postalCode: "81371"
+            }
+        }).save();
+    }).then(function() {
+        //add vehicle
+        const vehicle = new model.vehicle({
+            maxDistance: 10,
+            maxSize: 15,
+            maxItems: 8
+        });
+        vehicle.save().then(function (veh) {
+            const driver = new model.driver({driverLicenseNumber: "abcde12345", isAvailable: true, vehicle: veh._id});
+            driver.save().then(function (driv) {
+                const seppDriver = new model.user({
+                    name: "Sepp",
+                    lastName: "Müller",
+                    birthday: new Date('1995-05-08T03:24:00'),
+                    homeAddress: {
+                        city: "Muenchen",
+                        street: "Implerstraße",
+                        houseNumber: 1,
+                        postalCode: "81371"
+                    }
                 });
-                return route.save();
-            }).then(() => {
-                model.route.find().then((goods) => {
-                    console.log(goods);
-                }).catch(handleErr);
-            }).catch(handleErr);
+                seppDriver.driver = driv._id;
+                seppDriver.save().then(function (seppUser) {
+                    const route = new model.route({
+                        kilometers: 10,
+                        estimatedArrivalTimes: [new Date('2019-06-20T03:24:00')],
+                        items: [dishwasher, bike],
+                        auctionBids: [{
+                            owner: driver._id,
+                            bid: 4,
+                            timestamp: new Date('2019-06-19T03:24:00')
+                        }, {
+                            owner: driver._id,
+                            bid: 3,
+                            timestamp: new Date('2019-06-19T04:24:00')
+                        }]
+                    });
+                    return route.save();
+                }).then(() => {
+                    model.route.find().then((goods) => {
+                        console.log(goods);
+                    });
+                });
 
-        }).catch(handleErr);
+            });
+        });
     }).catch(handleErr);
-
 };
 
 module.exports = executeTest;
