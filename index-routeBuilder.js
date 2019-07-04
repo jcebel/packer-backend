def584e2f3b5b261ee7c306bbac7b33c5493c35b
Jonas Ebel <jonas.ebel@tum.de>
2019-07-04 14:35:41 +0200

    Prepare Development with TODOS and Test Data

--- index-routeBuilder.js
+++ index-routeBuilder.js
@@ -6,3 +6,23 @@ const buildingDate = process.argv[2] ? new Date(process.argv[2]) : new Date(new 
 
 console.log("% Build Route For: " + buildingDate.toDateString() + " %");
 
+/**
+ *
+ * Pseudo Code:
+ *
+ * 1. TODO Load all Delivery Items for given Date
+ * 2. TODO Extract all Start Positions into one Array
+ * 3. TODO Hand this Array to GoogleService -> Returns Distance Matrix for all Start Positions.
+ * 4. TODO Use a hierarchical clustering algorithm to separate Start points into different Route
+ * 5. For each Collection:
+ *      5.1 TODO Order Starts based on shorted distance -> Start Array
+ *      5.2 TODO Create Route Object with Dist, Items, collect, date
+ *      5.3 TODO Collect All Ends Belonging to this start collection -> Array with end points
+ *      5.4 TODO Hand this to GoogleService -> Distance Matrix for End Position.
+ *      5.5 TODO GoogleService.dist(Collect[collect.length - 1],all End Positions) -> Return Dist_Start-End
+ *      5.6 TODO Order End points beginning with lowest value in Dist_Start-End -> Array of end Points
+ *      5.7 TODO Add array to Route.deliver
+ *      5.8 TODO Route.vehicleType = VehicleTypeService.vehicleRecommendation(route)
+ *      5.9 TODO mongodb.save(route)
+ * END
+ */
---
+++ src-test/mock-distanceMatrix.js
@@ -0,0 +1,73 @@
+module.export = {
+  destination_addresses: [
+    'Garching - Forschungszentrum, 85748 Garching bei München, Germany',
+    'Implerstraße 12A, 81371 München, Germany',
+    'Mies-van-der-Rohe-Straße 6d, 80807 München, Germany'
+  ],
+  origin_addresses: [
+    'Garching - Forschungszentrum, 85748 Garching bei München, Germany',
+    'Implerstraße 12A, 81371 München, Germany',
+    'Mies-van-der-Rohe-Straße 6d, 80807 München, Germany'
+  ],
+  rows: [
+    {
+      elements: [
+        {
+          distance: { text: '1 m', value: 0 },
+          duration: { text: '1 min', value: 0 },
+          status: 'OK'
+        },
+        {
+          distance: { text: '25.4 km', value: 25427 },
+          duration: { text: '28 mins', value: 1709 },
+          status: 'OK'
+        },
+        {
+          distance: { text: '14.2 km', value: 14220 },
+          duration: { text: '13 mins', value: 784 },
+          status: 'OK'
+        }
+      ]
+    },
+    {
+      elements: [
+        {
+          distance: { text: '24.5 km', value: 24458 },
+          duration: { text: '27 mins', value: 1631 },
+          status: 'OK'
+        },
+        {
+          distance: { text: '1 m', value: 0 },
+          duration: { text: '1 min', value: 0 },
+          status: 'OK'
+        },
+        {
+          distance: { text: '11.9 km', value: 11895 },
+          duration: { text: '19 mins', value: 1158 },
+          status: 'OK'
+        }
+      ]
+    },
+    {
+      elements: [
+        {
+          distance: { text: '14.5 km', value: 14489 },
+          duration: { text: '14 mins', value: 830 },
+          status: 'OK'
+        },
+        {
+          distance: { text: '12.1 km', value: 12146 },
+          duration: { text: '19 mins', value: 1131 },
+          status: 'OK'
+        },
+        {
+          distance: { text: '1 m', value: 0 },
+          duration: { text: '1 min', value: 0 },
+          status: 'OK'
+        }
+      ]
+    }
+  ],
+  status: 'OK'
+}
+
