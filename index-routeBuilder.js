133bad97d324a4b857f07f6e3a9b84450966ce8b
Jonas Ebel <jonas.ebel@tum.de>
2019-07-08 16:43:13 +0200

    Calculate Distance And Duration

--- index-routeBuilder.js
+++ index-routeBuilder.js
@@ -11,8 +11,15 @@ const mock_google = require('./src-test/mock-distanceMatrix');
 console.log("%       Starting Route Builder      %");
 
 const buildingDate = process.argv[2] ? new Date(process.argv[2]) : new Date(new Date().toDateString());
-
+const driverStartDate = process.argv[3] ? new Date(process.argv[3]) : new Date(buildingDate);
+if (!process.argv[3]) {
+    driverStartDate.setHours(18);
+}
+console.log("%                                   %");
 console.log("%  Build Route For: " + buildingDate.toDateString() + " %");
+
+console.log("%    Delivery starts at " + driverStartDate.toLocaleTimeString() + "    %");
+console.log("%       Starting Route Builder      %");
 
 function createTestDataItem() {
 //TODO Delete this.
@@ -60,7 +67,7 @@ function createTestDataItem() {
  *      5.5 GoogleService.dist(Collect[collect.length - 1],all End Positions) -> Return Dist_Start-End
  *      5.6 Order End points beginning with lowest value in Dist_Start-End -> Array of end Points
  *      5.7 Add array to Route.deliver
- *          5.7.1 TODO Calculate ETA, Distance and Weight.
+ *          5.7.1 Calculate ETA, Distance.
  *      5.8 TODO Route.vehicleType = VehicleTypeService.vehicleRecommendation(route)
  *      5.9 TODO mongodb.save(route)
  *      5.10 TODO All Items need to be marked as Routed (In Route Model AND in the DeliveryGOOD Model)
@@ -88,12 +95,12 @@ function createTestDataItem() {
     }
 
     function retrieveDistance(a, b, distStruct, distMatrix) {
-        return distMatrix.rows[distStruct[a._id].position].elements[distStruct[b._id].position].duration.value
+        return distMatrix.rows[distStruct[a._id].position].elements[distStruct[b._id].position].distance.value
     }
 
     function clusterHierarchical(items, distanceMatrixStart, distStruct) {
 
-        const distance = (a, b) => retrieveDistance(a, b, distStruct, distanceMatrixStart);
+        const distance = (a, b) => retrieveTime(a, b, distStruct, distanceMatrixStart);
         // single linkage according to docs.
         const linkage = (distances) => Math.min.apply(null, distances);
         const levels = cluster({
@@ -105,39 +112,56 @@ function createTestDataItem() {
         return mapLevelsToData(levels, items);
     }
 
+    /**
+     Returns Time in Seconds
+     */
+    function retrieveTime(a, b, distStruct, distanceMatrix) {
+        return distanceMatrix.rows[distStruct[a._id].position].elements[distStruct[b._id].position].duration.value;
+    }
+
     function sortItemsByDistance(items, distanceMatrix, distStruct, indexOfFirstItem) {
         let item = items[indexOfFirstItem ? indexOfFirstItem : 0];
+        let duration = 0;
+        let totalDistance = 0;
         const sortedItems = [];
         for (let i = 0; i < items.length; i++) {
+            let oldItem = item;
             distStruct[item._id].alreadyVisited = true;
-            sortedItems.push(item);
+            sortedItems.push(oldItem);
             item = items.reduce((total, otherItem) => {
                 if (item._id != otherItem && !distStruct[otherItem._id].alreadyVisited) {
-                    let distance = retrieveDistance(item, otherItem, distStruct, distanceMatrix);
+                    let distance = retrieveTime(item, otherItem, distStruct, distanceMatrix);
                     return distance < total.dist ? {item: otherItem, dist: distance} : total;
                 }
                 return total;
             }, {dist: Number.MAX_VALUE}).item;
+            if (item) { //Fence-post-problem: Only Calculate the Distance/Duration as the last item has not been reached.
+                duration += retrieveTime(oldItem, item, distStruct, distanceMatrix);
+                totalDistance += retrieveDistance(oldItem, item, distStruct, distanceMatrix);
+            }
+            console.log(duration)
         }
-        return sortedItems;
+        return {sortedItems: sortedItems, duration: duration, totalDistance: totalDistance};
     }
 
     await mongoose.connect(config.mongoURI, {useNewUrlParser: true});
     const allItems = await model.deliveryGood.find().byDate(buildingDate);
     console.log("%      found " + allItems.length + " items for routing   %");
     /* TODO uncomment
-    const distanceMatrixStart = await GoogleService.getSquaredDistanceMatrix(allItems.map(
-        (item) => item.origination.toString()), 'driving'
-    );
+      const distanceMatrixStart = await GoogleService.getSquaredDistanceMatrix(allItems.map(
+          (item) => item.origination.toString()), 'driving'
+      );
+      console.log("Start");
+      log(distanceMatrixStart);
+      console.log("End");
+      const distanceMatrixEnd = await GoogleService.getSquaredDistanceMatrix(allItems.map(
+          (item) => item.destination.toString()), 'driving'
+      );
 
-    const distanceMatrixEnd = await GoogleService.getSquaredDistanceMatrix(allItems.map(
-        (item) => item.destination.toString()), 'driving'
-    );
-    */
+      log(distanceMatrixEnd);
+      */
     const distanceMatrixStart = mock_google.mockstart;
     const distanceMatrixEnd = mock_google.mockend;
-
-    // console.log(util.inspect(distStruct,false, null, true));
     const distStartStruct = buildDistanceStruct(allItems, distanceMatrixStart);
     const distEndStruct = buildDistanceStruct(allItems, distanceMatrixEnd);
 
@@ -145,43 +169,45 @@ function createTestDataItem() {
     console.log("%     Startpoints are being sorted  %");
     const routes = startCluster.map((deliveryItems, i) => {
         console.log("%     Size of Cluster " + i + ": " + deliveryItems.length + "          %");
-        const startAddresses = sortItemsByDistance(
-            deliveryItems, distanceMatrixStart, distStartStruct)
-            .map((item) => item.origination);
+        const sortResult = sortItemsByDistance(
+            deliveryItems, distanceMatrixStart, distStartStruct);
+        const startAddresses = sortResult.sortedItems.map((item) => item.origination);
         const route = new model.route(
             new model.route({
                 date: buildingDate,
                 items: deliveryItems,
+                estimatedTime: sortResult.duration,
+                kilometers: sortResult.totalDistance,
                 collect: startAddresses
             }));
         return route;
     });
-    console.log("%     First Endpoint is calculated  %");
+    console.log("%   First Endpoint is calculated    %");
 
-    // TODO remove
+    /* TODO remove*/
     for (let i = 0; i < routes.length; i++) {
         let route = routes[i];
         let distanceMatrixStartEnd = mock_google.start_to_end[i];
-        /* TODO Uncomment
-        for (let route of routes) {
-
-
-             const distanceMatrixStartEnd = await GoogleService.getDistanceMatrix(route.collect[route.collect.length - 1].toString(),
-                route.items.map((item) => item.destination.toString()), 'driving');*/
-
+    /* TODO Uncomment
+    for (let route of routes) {
+        const distanceMatrixStartEnd = await GoogleService.getDistanceMatrix(route.collect[route.collect.length - 1].toString(),
+            route.items.map((item) => item.destination.toString()), 'driving'); */
+        log(distanceMatrixStartEnd);
         let firstEndpoint = distanceMatrixStartEnd.rows[0].elements.reduce(
             (total, entry, index) => total.currentDuration > entry.duration.value ? {
                 currentDuration: entry.duration.value,
+                currentDistance: entry.distance.value,
                 itemPosition: index
             } : total,
             {currentDuration: Number.MAX_VALUE});
 
-        //TODO Merge Route.destination mapping with googleService call..
-        route.deliver = sortItemsByDistance(route.items, distanceMatrixEnd, distEndStruct, firstEndpoint.itemPosition)
-            .map((item) => item.destination);
+        let sortResult = sortItemsByDistance(route.items, distanceMatrixEnd, distEndStruct, firstEndpoint.itemPosition);
+        route.kilometers += firstEndpoint.currentDistance + sortResult.totalDistance;
+        route.estimatedTime += firstEndpoint.currentDuration + sortResult.duration;
+        route.deliver = sortResult.sortedItems.map((item) => item.destination);
+        log(route);
+        console.log("%Successfully calculated collection %");
 
-        console.log("%Successfully calculated collection%");
-        console.log(route);
 
     }
 
--- src-test/mock-distanceMatrix.js
+++ src-test/mock-distanceMatrix.js
@@ -225,7 +225,7 @@ const mockend = {
         }
     ],
     status: 'OK'
-}
+};
 
 
 const start_toEnd1 = {
