6a2b5eb56cf4abdcb197c3525519af2f3aaa266a
Lukas <lukas.vorwerk@tum.de>
2019-07-11 22:56:21 +0200

    process checkbox input in authController

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -4,6 +4,8 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const config = require('../config');
 const UserModel = require('../models/User');
+const DeliveryClientModel = require('../models/DeliveryClient');
+const DriverModel = require('../models/Driver');
 
 const register = (req,res) => {
     if (!Object.prototype.hasOwnProperty.call(req.body, 'password')) return res.status(400).json({
@@ -16,8 +18,21 @@ const register = (req,res) => {
         message: 'The request body must contain an email property'
     });
 
-    const user = Object.assign(req.body, {password: bcrypt.hashSync(req.body.password, 8)});
+    for(let i=0; i<req.body.checkboxIds.length; i++){
+        if(req.body.checkboxIds[i] === "deliveryClient"){
+            const delClient = new DeliveryClientModel();
+            delClient.save();//TODO: Sollte man hier ein Promise machen?
+            req.body.deliveryClient = delClient._id;
+        }
+        if(req.body.checkboxIds[i] === "driver"){
+            const driver = new DriverModel();
+            driver.save();//TODO: Sollte man hier ein Promise machen?
+            req.body.driver = driver._id;
+        }
+    }
+    delete req.body.checkboxIds;
 
+    const user = Object.assign(req.body, {password: bcrypt.hashSync(req.body.password, 8)});
 
     UserModel.create(user)
         .then(user => {
@@ -30,7 +45,7 @@ const register = (req,res) => {
 
         })
         .catch(error => {
-            if(error.code == 11000) {
+            if(error.code === 11000) {
                 res.status(400).json({
                     error: 'User exists',
                     message: error.message
