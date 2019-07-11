fbecdffda94bda551f2c887e08516150876e5312
MaxiPfeiler <maximilian.pfeiler@tum.de>
2019-07-11 10:39:30 +0200

    added logic

--- src/services/priceCalculation.js
+++ src/services/priceCalculation.js
@@ -7,9 +7,29 @@ const priceCalculation = (req, res) => {
 
     var price = 1;
 
-    
+    if(size == "Small") {
+        price = price + 1;
+    } else if(size == "Medium") {
+        price = price + 2;
+    } else if(size == "Large") {
+        price = price + 3;
+    }
+    if(weight == "Light") {
+        price = price + 1;
+    } else if(weight == "Medium") {
+        price = price + 2;
+    } else if (weight == "Heavy") {
+        price = price + 3;
+    }
+    if(distance <= 8 ) {
+        price = price + 1;
+    } else if(distance > 8 && distance <= 16) {
+        price = price + 2;
+    } else if (distance > 16) {
+        price = price + 3;
+    }
 
-    res.status(200).json({price : 0});
+    res.status(200).json({price : price});
 };
 
 module.exports = {priceCalculation};
