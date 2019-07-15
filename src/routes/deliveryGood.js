d9cc4a0c7614f23ea99870c8fa7cf7be9746ab50
Lukas <lukas.vorwerk@tum.de>
2019-07-15 13:55:30 +0200

    adapt test class to delivery details view

--- src/routes/deliveryGood.js
+++ src/routes/deliveryGood.js
@@ -2,7 +2,6 @@
 
 const express  = require('express');
 const router   = express.Router();
-const middleware = require('../middleware');
 const DeliveryGoodController = require('../controllers/deliveryGood');
 const middleware    = require('../middleware');
 
