3efe45945dacbba0e9ee986ca2832e8511e4a633
Jonas Ebel <jonas.ebel@tum.de>
2019-07-18 09:55:59 +0200

    enable routes of today's list

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,8 +6,7 @@ const middleware = require('../middleware');
 
 const RouteController = require('../controllers/route');
 
-router.get('/', middleware.checkAuthentication, RouteController.list);//List all routes //TODO Delete
-router.get('/byDate/:date', RouteController.listByDate); //List by date
+router.get('/ofToday', RouteController.listOfToday); //list Of Today's Routes
 router.get('/:id', middleware.checkAuthentication, RouteController.read);//Search for route by id
 router.put('/:id',middleware.checkAuthentication, RouteController.updateBid);//add new bid to route
 
